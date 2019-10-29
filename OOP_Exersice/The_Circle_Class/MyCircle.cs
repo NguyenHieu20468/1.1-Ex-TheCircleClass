a484531d3e24ccd4865088806664a0121a60d2c7
Thanhnhanss <@vanlanguni.vn>
2019-10-29 14:26:13 +0700

    Doing

---
+++ OOP_Exersice/The_Circle_Class/MyCircle.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Circle_Class
+{
+    public class MyCircle
+    {
+        private double radius;
+        private string color;
+
+        public MyCircle()
+        {
+            radius = 1.0;
+            color = "red";
+        }
+
+        public MyCircle( double r)
+        {
+            radius = r;
+            color = "red";
+        }
+
+        public MyCircle(double r, string c )
+        {
+            radius = r;
+            color = c;
+        }
+
+        public double GetRadius()
+        {
+            return radius;
+        }
+        public double GetAre()
+        {
+            return radius * radius * Math.PI;
+        }
+
+        public string GetColor()
+        {
+            return color;
+        }
+
+        public void SetRadius(double newRadius)
+        {
+            radius = newRadius;
+        }
+
+        public void SetColor(string newColor)
+        {
+            color = newColor;
+        }
+    }
+}
---
+++ OOP_Exersice/The_Circle_Class/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Circle_Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyCircle circle = new MyCircle();
+            Console.WriteLine("The circle has radius of {0} and get arra is {1} ", circle.GetRadius(), circle.GetAre());
+
+            MyCircle circle1 = new MyCircle(5);
+            Console.WriteLine("The circle has radius of {0} and get arra is {1} ", circle1.GetRadius(), circle1.GetAre());
+
+            MyCircle circle2 = new MyCircle(2, "Orange");
+            Console.WriteLine("The circle has radius of {0} and get arra is {1} and set color is {2} ", circle2.GetRadius(), circle2.GetAre(),circle2.GetColor());
+        }
+    }
+}
